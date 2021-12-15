f1dca938fdd956f79b593827bfed503b49a127b0
paulinhorozendo <81476932+paulinhorozendo@users.noreply.github.com>
2021-12-15 15:09:13 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# api-restful
-API RESTful com Node.js e MongoDB | CRUD com Node, Express e Mongoose
+## API REST com Node.js e MongoDB
+
+# API baseada no framework Express para tratar as requisições e enviar as respostas aos endpoints;
+
+# Utilizado o MongoDB como banco, porém com a ODM Mongoose;
+
+# Operações realizadas no MongoDB Atlas (https://cloud.mongodb.com/);
+
+# O pacote Nodemon ajuda a atualizar o código da aplicação em tempo real;
+
+# A API testada pelo software Postman (https://www.postman.com);
---
+++ index.js
@@ -0,0 +1,40 @@
+// config inicial
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// forma de ler JSON / middlewares
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+// rotas daAPI
+const personRoutes = require("./routes/personRoutes");
+
+app.use("/person", personRoutes);
+
+// rota inicial / endpoint
+app.get("/", (req, res) => {
+  // mostrar req
+
+  res.json({ message: "olá Express!" });
+});
+
+// entregar uma porta
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.p9qgx.mongodb.net/bancodaapi?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Conectamos ao MongoDB!");
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
